1e68768ab5ba6775d3a28a8b55967bf9ab466935
n-q-m-e <tessonlucas@gmail.com>
2021-05-24 13:14:24 +0200

    Choix conversation ajouté

--- app/src/main/java/com/example/chat2021/LoginActivityMD.java
+++ app/src/main/java/com/example/chat2021/LoginActivityMD.java
@@ -116,7 +116,7 @@ public class LoginActivityMD extends AppCompatActivity implements View.OnClickLi
                 inputLogin.setErrorEnabled(false);
                 inputPasse.setErrorEnabled(false);
                 LoginActivityMD.this.alerter(hash);
-                Intent iVersChoixConv = new Intent(LoginActivityMD.this,ChoixConvActivity.class);
+                Intent iVersChoixConv = new Intent(LoginActivityMD.this,ChoixConvActivityMD.class);
                 Bundle bdl = new Bundle();
                 bdl.putString("hash",hash);
                 iVersChoixConv.putExtras(bdl);
